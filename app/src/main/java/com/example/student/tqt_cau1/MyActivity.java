38a073d0f2cfff7dfb4a0de5508a5831e4729257
tranquangthinh <tranquangthinhtk@gmail.com>
2019-09-09 17:02:39 +0700

    Lam cau 2

--- app/src/main/java/com/example/student/tqt_cau1/MyActivity.java
+++ app/src/main/java/com/example/student/tqt_cau1/MyActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.tqt_cau1;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,7 @@ import android.widget.Toast;
 public class MyActivity extends AppCompatActivity {
 
     Button btnthoat,dangNhap;
-    CheckBox cb;
+    CheckBox cb,btnThoat;
     int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,30 @@ public class MyActivity extends AppCompatActivity {
                 }else{
                     Toast.makeText(MyActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnthoat = (Button) findViewById(R.id.btnThoat);
+        btnthoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder mydialog = new AlertDialog.Builder(MyActivity.this);
+                mydialog.setTitle("Thông báo");
+                mydialog.setMessage("Bạn muốn thoát không?");
+                mydialog.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                mydialog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog alertDialog = mydialog.create();
+                alertDialog.show();
             }
         });
     }
